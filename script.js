e12eb67eec68a85e08ba0ec0621ae69746a0dddc
Carles Tur <carlesturcardona@gmail.com>
2024-01-21 20:32:44 +0100

    Añado animación al botón del aside

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function toggleSidebar() {
     var container = document.querySelector('.contenedor_lenguajes');
     container.classList.toggle('open');
+    var toggleBtn = document.querySelector('.toggle-btn');
+    toggleBtn.classList.toggle('open');
 }
