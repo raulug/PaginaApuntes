46c03d13d4e3f36df01af19dd5872663be4329e2
Carles Tur <carlesturcardona@gmail.com>
2024-01-21 23:16:42 +0100

    Añado funcionalidad desplegable al indice (falta arreglarlo) y corrijo todos los html de cada lenguaje

--- script.js
+++ script.js
@@ -4,3 +4,33 @@ function toggleSidebar() {
     var toggleBtn = document.querySelector('.toggle-btn');
     toggleBtn.classList.toggle('open');
 }
+
+function toggleIndice(icon) {
+    // Encuentra el elemento li padre
+    var parentLi = icon.closest('li');
+
+    // Encuentra la lista de índice específica dentro del li actual
+    var temaLista = parentLi.querySelector('.indice');
+
+    // Abre o cierra el índice correspondiente
+    if (temaLista) {
+        temaLista.classList.toggle('open');
+
+        // Encuentra el icono dentro del div
+        var indiceBtn = parentLi.querySelector('.indice-btn');
+
+        // Cambia el icono según el estado de la lista
+        if (temaLista.classList.contains('open')) {
+            indiceBtn.classList.add('open');
+        } else {
+            indiceBtn.classList.remove('open');
+        }
+    }
+}
+
+
+
+
+
+
+
